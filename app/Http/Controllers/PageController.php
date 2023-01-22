8d9250f0d58aa2624a3aa7d2d4b699137eccc197
giogua03 <giorgiogua03@gmail.com>
2023-01-22 16:03:52 +0100

    finito

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,7 +9,7 @@ class PageController extends Controller
 {
    public function index() {
         $movies = Movie::all();
-        
+        // dd($movies);
         return view('home', compact('movies'));
    } //
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>FILM</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-<body>
-    <h1>HOME</h1>
-    @foreach ($movies as $movie)
-        <ul>
-            <li><h3>{{ $movie->title }}</h3></li>
-            <span >titolo in lingua originale :</span>
-            <span class="or">{{ $movie->original_title }}</span>
-        </ul>
-    @endforeach
-</body>
-</html>
+@extends('layouts.app')
+@section('content')
+    <h1 class="text-center">Movies</h1>
+    
+    <div class="container">
+        <div class="row">
+            @foreach ($movies as $movie)
+                    <div class="col-3 g-2">
+                        <div class="card bg-secondary" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">{{$movie['title']}}</h5>
+                                <p class="card-text">Nationality: {{$movie['nationality']}}</p>
+                                <p class="card-text">Date: {{$movie['date']}}</p>
+                                <p class="card-text">Date: {{$movie['vote']}}</p>
+                            </div>
+                        </div>
+                    </div>
+            @endforeach
+        </div>
+
+    </div>
+@endsection
